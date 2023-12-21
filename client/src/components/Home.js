907bce63594162688c8a813247f862433fbee5c9
Nickon Babaei <babaein1@gmail.com>
2023-12-20 20:27:39 -0400

    added header text

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,10 +2,20 @@ import React from 'react'
 import Nav from './Nav'
 
 const Home = () => {
-  return (
-    <div className='min-h-screen bg-gray-800'>
-    </div>
-  )
+    return (
+        <div className='min-h-screen w-screen bg-gray-800'>
+            <div className='max-w-screen-xl mx-auto'>
+
+                <div className='sm:text-6xl text-4xl font-medium text-white font-mono px-6 pt-4'>
+                    <h1 className='pb-4'>Work from the convenience of your own home.</h1>
+                    <h1>Anytime, Anywhere.</h1>
+                </div>
+
+                
+
+            </div>
+        </div>
+    )
 }
 
 export default Home
